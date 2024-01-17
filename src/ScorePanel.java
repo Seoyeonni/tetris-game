b46d0fea9120e0a5c7f438d38d5ff16db66f50ce
geomunoreum <143944871+geomunoreum@users.noreply.github.com>
2024-01-17 22:05:41 +0900

    Update ScorePanel.java

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -9,25 +12,35 @@ public class ScorePanel extends JPanel {
 
 	// 점수 관련 객체
 	private int score = 0; // 점수
-	private JLabel textLabel = new JLabel("SCORE"); // 점수 레이블 객체 생성
+	private JLabel textLabel = new JLabel(); // 점수 레이블 객체 생성
 	private JLabel scoreLabel = new JLabel(Integer.toString(score)); // scoreLabel 객체 생성
 
 	public ScorePanel() {
 		setLayout(null); // 배치관리자 삭제
+		
+		ImageIcon scoreImg= new ImageIcon("score_text.png"); //점수 이미지 생성
+		textLabel.setIcon(scoreImg);
 
 		// 점수 관련 객체 추가
-		textLabel.setForeground(new Color(0, 0, 0));
-		textLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
-		textLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		textLabel.setSize(50, 20);
-		textLabel.setLocation(50, 50);
+		textLabel.setBounds(35, 55, scoreImg.getIconWidth(), scoreImg.getIconHeight());
 		add(textLabel);
 
-		scoreLabel.setForeground(new Color(0, 0, 0));
-		scoreLabel.setFont(new Font("Monospaced", Font.BOLD, 15));
-		scoreLabel.setSize(50, 20);
-		scoreLabel.setLocation(110, 50);
+		scoreLabel.setForeground(Color.YELLOW);
+		scoreLabel.setFont(new Font("Monospaced", Font.BOLD, 30));
+		//scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		scoreLabel.setSize(100, 20);
+		scoreLabel.setLocation(70, 100);
 		add(scoreLabel);
+	}
+	
+	@Override //배경
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		ImageIcon bg = new ImageIcon("score_background.png");
+		Image img= bg.getImage();
+		
+		g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
 	}
 
 	// 점수 설정 함수
